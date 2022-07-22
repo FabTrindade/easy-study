ae7f17a53df652df4a83dc8f21d191d132c3bc61
Fabricio Trindade <fabrtrindade@gmail.com>
2022-07-22 18:37:01 -0300

    Usando CSS Modules

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Botao from '../botao';
-import './style.scss';
+import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
 
     render() {
         return (
-            <form className='novaTarefa'>
-                <div className='inputContainer'>
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione um novo estudo
                     </label>
@@ -20,7 +20,7 @@ class Formulario extends React.Component {
                     />
                 </div>
 
-                <div className='inputContainer'>
+                <div className={style.inputContainer}>
                     <label htmlFor='tempo'>
                         Tempo
                     </label>
