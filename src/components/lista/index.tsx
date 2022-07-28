eff7f0b2d425cd8d89bb3a21c1abb70bf705eb81
Fabricio Trindade <fabrtrindade@gmail.com>
2022-07-28 19:03:50 -0300

    Introducing states with Hook useStates

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './item';
 import style from './Lista.module.scss'
 
 function Lista() {
-    const tarefas = [
+    const [tarefas, setTarefas] = useState([
         {
             tarefa: 'React',
             tempo: '02:00:00'
@@ -11,11 +11,15 @@ function Lista() {
         {
             tarefa: 'JavaScript',
             tempo: '01:00:00'
-        }];
+        }
+    ])
+        
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTarefas ([...tarefas, {tarefa: 'Estudar estado', tempo: '05:00:00'}])
+            }}>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item key={index} {...item}/>
