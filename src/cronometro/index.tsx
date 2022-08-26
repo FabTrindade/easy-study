85d41b6fda563baffd3485a65e29fb5da8f32c6a
Fabricio Trindade <fabrtrindade@gmail.com>
2022-08-26 19:44:04 -0300

    Showing on the clock

--- src/cronometro/index.tsx
+++ src/cronometro/index.tsx
@@ -9,20 +9,19 @@ interface Props {
     selecionado: ITarefa | undefined
 }
 
-export default function Cronometro({selecionado}:Props){
+export default function Cronometro({ selecionado }: Props) {
     const [tempo, setTempo] = useState<number>();
-    useEffect( () => {
-        if (selecionado?.tempo)
-        {
-            setTempo (tempoParaSegundos(selecionado.tempo));
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo));
         }
     }, [selecionado]);
-    
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
             <div className={style.relogioWrapper}>
-               <Relogio /> 
+                <Relogio tempo={tempo} />
             </div>
             <Botao>
                 Começar!
