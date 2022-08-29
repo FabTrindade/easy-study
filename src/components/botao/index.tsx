33a7a01d79a700cc76afd3ff7f1bc74b44ee2eff
Fabricio Trindade <fabrtrindade@gmail.com>
2022-08-29 19:49:11 -0300

    Chromometer countdown implemented

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import style from './Botao.module.scss'
 
-class Botao extends React.Component <{
-     children: any 
-     type?: "button" | "submit" | "reset" | undefined
-    }>{    
-    render () {
-        const {type = 'button'} = this.props;
+class Botao extends React.Component<{
+    children: any
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void | undefined
+}>{
+    render() {
+        const { type = 'button', onClick } = this.props;
         return (
-            <button type={type} className={style.botao}>
+            <button
+                type={type} 
+                className={style.botao}
+                onClick={onClick}>
                 {this.props.children}
             </button>
         )
